01be3bce5d9ecb3a9675bf61c2814510891e739c
s6530613006 <kuroomi10223@gmail.com>
2023-11-16 10:56:50 +0700

    second

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const RESTAPI = "http://localhost:3000/api"
+const RESTAPI = "https://homework5backend.vercel.app/api"
 // const RESTAPI = "http://172.26.117.16:3000/api/"
 const api = axios.create({ 
   baseURL: RESTAPI,
